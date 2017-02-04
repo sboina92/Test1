a9c97ac1691174e15349b1c6ec8dff4143f01f17
varun <rmotay@mail.bradley.edu>
2017-02-04 17:45:45 -0500

    included some code

--- src/com/testing/close/Simple.java
+++ src/com/testing/close/Simple.java
@@ -3,5 +3,8 @@ package com.testing.close;
 /**
  * Created by srikanth on 2/4/2017.
  */
-public class Simple {
+public  class Simple {
+    public static int mul(int a, int b){
+        return a*b;
+    }
 }
--- src/com/testing/run/Simple2.java
+++ src/com/testing/run/Simple2.java
@@ -1,7 +1,14 @@
 package com.testing.run;
 
+import com.testing.close.Simple;
+
 /**
  * Created by srikanth on 2/4/2017.
  */
 public class Simple2 {
+    public static void main(String[] args) {
+       // Simple s = new Simple();
+        int result = Simple.mul(10,20);
+        System.out.println("The Mul Vlaue is:"+ result);
+    }
 }
