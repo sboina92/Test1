ce02baeb67cb0454e998e1400e9ec58c0aef1ccf
varun <rmotay@mail.bradley.edu>
2017-02-04 18:14:37 -0500

    111124

--- src/com/testing/close/Simple.java
+++ src/com/testing/close/Simple.java
@@ -1,9 +1,13 @@
 package com.testing.close;
 
+
+import com.testing.stop.Stop;
+
 /**
  * Created by srikanth on 2/4/2017.
  */
 public  class Simple {
+    Stop s;
     public static int mul(int a, int b){
         return a*b;
     }
--- src/com/testing/stop/Stop.java
+++ src/com/testing/stop/Stop.java
@@ -3,5 +3,5 @@ package com.testing.stop;
 /**
  * Created by srikanth on 2/4/2017.
  */
-public class Simple {
+public class Stop {
 }
